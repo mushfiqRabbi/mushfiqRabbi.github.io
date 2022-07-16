a7a01808f1195540295f4d295afd965942f41d0d
mushfiq <mushfiqxrabbi@gmail.com>
2022-07-16 19:56:57 +0600

    done with mobile view too

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const docEl = document.documentElement;
 const btnEl = document.querySelector("button");
 const inEl = document.getElementById("input-email");
 const msg = document.querySelector(".msg");
+const imgCont = document.querySelector(".right");
+const elLeft = document.querySelector(".left");
+const imgContCopy = imgCont.cloneNode(true);
 
 btnEl.addEventListener("click", () => {
   if (inEl.value === "" || !inEl.checkValidity()) {
@@ -15,3 +18,25 @@ btnEl.addEventListener("click", () => {
     msg.classList.add("hidden");
   }
 });
+
+const mediaQueryM = window.matchMedia("(max-width: 415px)");
+const mediaQueryD = window.matchMedia("(min-width: 416px");
+
+function forDesktop(mq) {
+  if (mq.matches) {
+    imgCont.remove();
+    document.body.appendChild(imgContCopy);
+  }
+}
+
+function forMobile(mq) {
+  if (mq.matches) {
+    imgCont.remove();
+    elLeft.insertBefore(imgContCopy, elLeft.children[1]);
+  }
+}
+forDesktop(mediaQueryD);
+forMobile(mediaQueryM);
+
+mediaQueryD.addListener(forDesktop);
+mediaQueryM.addListener(forMobile);
